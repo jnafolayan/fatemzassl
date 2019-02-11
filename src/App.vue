3191e5c8d3af22fe2af11510942db15be73c9af1
jnafolayan <jnafolayan@gmail.com>
2019-02-11 18:41:09 +0100

    Add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,46 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <navbar></navbar>
+    <main>
+      <router-view></router-view>
+    </main>
+    <footer class="c-footer">
+      <div class="l-container">
+        <div class="c-footer__bottom">
+          <span class="c-footer__bottom-left">&copy; 2019</span>
+          <span class="c-footer__bottom-right">
+            Built by&nbsp;
+            <a href="https://jnafolayan.github.io">John Afolayan</a>
+          </span>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
 <style lang="scss">
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  font-size: 16px;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+
+  background: hsl(210, 9%, 15%);
+  min-height: 100vh;
 }
 </style>
+
+<script>
+  import Navbar from "./components/Navbar";
+
+  export default {
+    components: { Navbar }
+  }
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav class="c-navbar">
+    <div class="l-container">
+      <div class="c-navbar__header">
+        <a class="c-navbar__brand" href="/">Fatemza Space Solutions</a>
+        <button class="c-navbar__toggler">
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+      </div>  
+    </div>
+  </nav>
+</template>
+
+<style lang="scss">
+.c-header {
+  display: flex;
+}
+</style>
+
+<script>
+export default {
+  name: "navbar"
+}
+</script>
