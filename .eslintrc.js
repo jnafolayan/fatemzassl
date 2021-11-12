48a6fbcee72036078ba4f7f99e2af2f7d74c2d68
John Afolayan <jnafolayan@gmail.com>
2021-11-12 01:15:59 +0100

    site update

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,12 @@ module.exports = {
   env: {
     node: true
   },
-extends: ["plugin:vue/essential"/*, "@vue/prettier"*/],
+  extends: ["plugin:vue/essential"/*, "@vue/prettier"*/],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-trailing-spaces": "off"
+    "no-trailing-spaces": "off",
+    "vue/multi-word-component-names": "off"
   },
   parserOptions: {
     parser: "babel-eslint"
